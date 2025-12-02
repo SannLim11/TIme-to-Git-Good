05cd773635b258cef134b81e7db4e4770acc24c0
SannLim11 <limzhiying08@gmail.com>
2025-12-02 20:43:05 +0800

    corrected ver

---
+++ Guess2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    const int MIN = 0, MAX = 100;
+    cout << "=== Welcome to the Number Guessing Game! ===\n";
+
+    srand(time(0));
+    int number = rand() % (MAX - MIN ) + MIN;
+
+    int guess, attempts = 0;
+
+    while (guess != number) 
+    {
+        cout << "Guess a number between " << MIN << " and " << MAX << ": ";
+        cin >> guess;
+        
+        attempts =+ 1;
+
+        if (guess < number)
+            cout << "Too low! Try again.\n";
+        else if (guess > number)
+            cout << "Too high! Try again.\n";
+        else {
+            cout << "Congratulations! You guessed the number " << number << " in " << attempts << " attempts.\n";
+        }
+        
+    }
+
+    cout << "---------------------------------\n";
+
+    system("pause");
+    return 0;
+}
